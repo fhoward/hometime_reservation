c5380aa1a13972f92845f6573292b401c7e4f622
francis howard susa <francishoward28@gmail.com>
2025-02-02 23:55:51 +0800

    - include app/services - disable wrap_parameters_by_default

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,10 @@ module HometimeReservationApi
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    # Include App Service
+    config.eager_load_paths << Rails.root.join("app/services")
+    # Disable wrapper
+    config.action_controller.wrap_parameters_by_default = false
 
     # Configuration for the application, engines, and railties goes here.
     #
